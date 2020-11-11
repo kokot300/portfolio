e0a043f0acda4bb35ecf30440e478cecbd17fd11
kokot300 <kokot300@gmail.com>
2020-11-11 11:11:16 +0100

    storypoints 6,7,8 done

--- clothes/wardrobe/admin.py
+++ clothes/wardrobe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Institution, Donation, Category
+
 # Register your models here.
+
+admin.site.register(Institution)
+admin.site.register(Donation)
+admin.site.register(Category)
